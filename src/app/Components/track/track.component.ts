02a24e05b5f3a2ce1d554f2f5e1fb078220bd147
mbbrill <mbbrill@yahoo.com>
2023-09-21 09:51:18 -0700

    website error

--- src/app/Components/track/track.component.ts
+++ src/app/Components/track/track.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, OnInit, EventEmitter } from "@angular/core";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { Track } from "src/app/home/home.component";
-
+import { Howl } from 'howler';
 
 @Component({
   selector: "app-track",
@@ -13,14 +13,39 @@ export class TrackComponent implements OnInit {
   @Input() isActive: boolean = false;
   @Input() index!: number;
   @Output() trackClicked = new EventEmitter<void>();
-  constructor() {}
 
-  ngOnInit(): void {}
+  private sound: Howl | undefined = undefined;
+  private isPlaying: boolean = false;
+
+
+  constructor() {
+
+   
+
+  }
+
+  ngOnInit(): void {
+
+    this.sound = new Howl({
+      src: [this.track.trackPreview], // Replace with your audio file path
+      html5: true,
+    volume: 0.5
+    });
+
+  }
 
   faPlay = faPlay;
 
   onClick() {
     this.trackClicked.emit();
-    console.log(this.track.trackPreview);
+    console.log(this.track.trackPreview)
+    console.log(this.sound)
+    if(this.sound?.playing()){
+      this.sound.pause()
+    }else{
+      this.sound?.play()
+    }
+    
+
   }
 }
